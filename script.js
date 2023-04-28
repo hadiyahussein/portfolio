8351e9c2a33659bc9cd0d6358fabf623cdeda146
Hadiya Hussein <you@example.com>
2023-04-28 18:14:08 -0400

    fixed scroll animation

--- script.js
+++ script.js
@@ -1,46 +1,52 @@
-// Navigation slide animation
+
+// Hamburger Menu Animation
 const navSlide = () => {
-    const burger = document.querySelector('.hamburger');
-    const nav = document.querySelector('.social-link');
-    const navLinks = document.querySelectorAll('.social-link a');
-  
-    burger.addEventListener('click', () => {
-      nav.classList.toggle('nav-active');
-  
-      navLinks.forEach((link, index) => {
-        if (link.style.animation) {
-          link.style.animation = '';
-        } else {
-          link.style.animation = `navLinkFade 0.3s ease forwards ${index / 7 + 0.3}s`;
-        }
-      });
-  
-      // Hamburger Animation
-      burger.classList.toggle('toggle');
-  
-      const lines = burger.querySelectorAll('.line');
-      lines.forEach((line) => {
-        line.classList.toggle('toggle');
-      });
-    });
-  };
-  
-  // Box reveal animation
-  const boxes = document.querySelectorAll('.box');
-  const checkBoxes = () => {
-    const triggerBottom = (window.innerHeight / 5) * 4;
-    boxes.forEach((box) => {
-      const boxTop = box.getBoundingClientRect().top;
-      if (boxTop < triggerBottom) {
-        box.classList.add('show');
+  const burger = document.querySelector('.hamburger');
+  const nav = document.querySelector('.social-link');
+  const navLinks = document.querySelectorAll('.social-link a');
+
+  burger.addEventListener('click', () => {
+    nav.classList.toggle('nav-active');
+
+    navLinks.forEach((link, index) => {
+      if (link.style.animation) {
+        link.style.animation = '';
       } else {
-        box.classList.remove('show');
+        link.style.animation = `navLinkFade 0.3s ease forwards ${index / 7 + 0.3}s`;
       }
     });
-  };
-  
-  // Event listeners
-  window.addEventListener('scroll', checkBoxes);
-  checkBoxes();
-  navSlide();
-  
+
+    // Hamburger Animation
+    burger.classList.toggle('toggle');
+
+    const lines = burger.querySelectorAll('.line');
+    lines.forEach((line) => {
+      line.classList.toggle('toggle');
+    });
+  });
+};
+
+// Scroll Animation
+const boxes = document.querySelectorAll('.box');
+const checkBoxes = () => {
+  const triggerBottom = (window.innerHeight / 5) * 4;
+  let allBoxesRevealed = true;
+  boxes.forEach((box) => {
+    const boxTop = box.getBoundingClientRect().top;
+
+    if (boxTop < triggerBottom) {
+      box.classList.add('show');
+    } else {
+      box.classList.remove('show');
+      allBoxesRevealed = false;
+    }
+  });
+  if (allBoxesRevealed) {
+    window.removeEventListener('scroll', checkBoxes);
+  }
+};
+
+// Event listeners
+window.addEventListener('scroll', checkBoxes);
+checkBoxes();
+navSlide();
