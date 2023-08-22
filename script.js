3838b4e07a3086c79acd198b801d547fb0a802fa
Hadiya Hussein <you@example.com>
2023-08-21 22:20:41 -0400

    updated portfolio

--- script.js
+++ script.js
@@ -1,39 +1,54 @@
+// Define professions and typing speed
+const professions = ["Hadiya <3", "a Front-end Developer", "a Ux Designer", "a Web Designer", "a Graphic Designer"];
+const speed = 100;
 
-// Hamburger Menu Animation
-const navSlide = () => {
-  const burger = document.querySelector('.hamburger');
-  const nav = document.querySelector('.social-link');
-  const navLinks = document.querySelectorAll('.social-link a');
+// Initialize profession index, profession element, and profession text
+let professionIndex = 0;
+const professionElement = document.getElementById("profession");
+let professionText = "";
 
-  burger.addEventListener('click', () => {
-    nav.classList.toggle('nav-active');
+// Function to type the profession
+function typeProfession() {
+  const profession = professions[professionIndex];
+  let index = 0;
 
-    navLinks.forEach((link, index) => {
-      if (link.style.animation) {
-        link.style.animation = '';
-      } else {
-        link.style.animation = `navLinkFade 0.3s ease forwards ${index / 7 + 0.3}s`;
-      }
-    });
+  const typeInterval = setInterval(() => {
+    professionText += profession.charAt(index);
+    professionElement.textContent = professionText;
+    index++;
 
-    // Hamburger Animation
-    burger.classList.toggle('toggle');
+    if (index === profession.length) {
+      clearInterval(typeInterval);
+      setTimeout(eraseProfession, 1500);
+    }
+  }, speed);
+}
 
-    const lines = burger.querySelectorAll('.line');
-    lines.forEach((line) => {
-      line.classList.toggle('toggle');
-    });
-  });
-};
+// Function to erase the profession
+function eraseProfession() {
+  let index = professionText.length;
+
+  const eraseInterval = setInterval(() => {
+    professionText = professionText.slice(0, -1);
+    professionElement.textContent = professionText;
+    index--;
+
+    if (index === 0) {
+      clearInterval(eraseInterval);
+      professionIndex = (professionIndex + 1) % professions.length;
+      setTimeout(typeProfession, 1000);
+    }
+  }, speed);
+}
 
 // Scroll Animation
-const boxes = document.querySelectorAll('.box');
-const checkBoxes = () => {
+function checkBoxes() {
+  const boxes = document.querySelectorAll('.box');
   const triggerBottom = (window.innerHeight / 5) * 4;
   let allBoxesRevealed = true;
+
   boxes.forEach((box) => {
     const boxTop = box.getBoundingClientRect().top;
-
     if (boxTop < triggerBottom) {
       box.classList.add('show');
     } else {
@@ -41,12 +56,41 @@ const checkBoxes = () => {
       allBoxesRevealed = false;
     }
   });
+
   if (allBoxesRevealed) {
     window.removeEventListener('scroll', checkBoxes);
   }
-};
+}
 
-// Event listeners
-window.addEventListener('scroll', checkBoxes);
-checkBoxes();
-navSlide();
+// Hamburger Menu Animation
+const navSlide = (() => {
+  const burger = document.querySelector('.hamburger');
+  const nav = document.querySelector('.social-link');
+  const navLinks = document.querySelectorAll('.social-link a');
+  const lines = burger.querySelectorAll('.line');
+
+  burger.addEventListener('click', () => {
+    nav.classList.toggle('nav-active');
+
+    navLinks.forEach((link, index) => {
+      link.style.animation = link.style.animation ? '' : `navLinkFade 0.3s ease forwards ${index / 7 + 0.3}s`;
+    });
+
+    // Hamburger Animation
+    burger.classList.toggle('toggle');
+    lines.forEach((line) => {
+      line.classList.toggle('toggle');
+    });
+  });
+})();
+
+// Check if the page is the home page and load the profession typing
+window.onload = function() {
+  checkBoxes();
+  window.addEventListener('scroll', checkBoxes);
+
+  // Check if the page is the home page and load the profession typing
+  if (window.location.pathname === '/index.html') {
+    typeProfession();
+  }
+};
